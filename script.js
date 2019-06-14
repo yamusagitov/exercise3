8c817edc16d965a4499c33bab23476db03fda623
IURII <yamusagitov@gmail.com>
2019-06-14 15:29:17 +0500

    exercise3_end

--- script.js
+++ script.js
@@ -19,10 +19,12 @@ let appData = {
     savings: true
 };
 
+let a, b;
+
 function chooseExpenses() {
     for (let i = 0; i < 2; i++) {
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-            b = prompt("Во сколько обойдется?", '');
+        a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = +prompt("Во сколько обойдется?", '');
 
         if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null &&
             a != '' && b != '' && a.length < 50) {
@@ -36,19 +38,10 @@ function chooseExpenses() {
 chooseExpenses();
 
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
+appData.moneyPerDay = (appData.budget / 30).toFixed(1);
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
-if (appData.moneyPerDay < 100) {
-    console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Произошла ошибка")
-}
 
 function checkSavings() {
     if (appData.savings == true) {
@@ -59,5 +52,46 @@ function checkSavings() {
         alert("Доход в месяц с Вашего депозита: " + appData.monthIncome);
     }
 }
-
 checkSavings();
+
+// Задание 3;
+
+// 1)
+function detectDayBudget() {
+    alert("Дневной бюджет: " + appData.moneyPerDay);
+}
+detectDayBudget();
+
+// 2)
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Минимальный уровень достатка");
+        alert ("Вы имеете минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+        alert("Вы имеете средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+        alert("Вы имеете высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка")
+    }
+}
+detectLevel();
+
+// 3)
+function chooseOptExpenses() {
+    for (let i = 0; i < 3; i++) {
+        let x = prompt("Статья необязательных расходов", ''),
+            y = +prompt("Во сколько обойдется?", '');
+
+        if ((typeof (x)) === 'string' && (typeof (x)) != null && (typeof (y)) != null &&
+            x != '' && y != '' && x.length < 50) {
+            console.log("done");
+            appData.optionalExpenses[x] = y;
+        } else {
+            i = i - 1;
+        }
+    };
+}
+chooseOptExpenses();
